2dcadb6fb1fd2cc6c043310c53019b5507b07f56
Ashraf Souleiman <Ashraf@cedrusco.com>
2017-11-30 10:37:48 -0800

    CZAR Commit

---
+++ customer/R1/controllers/v1/Client.js
@@ -0,0 +1,20 @@
+'use strict';
+var url = require('url');
+var Client = require('./ClientService');
+
+module.exports.getClient = function getClient (req, res, next) {
+    Client.getClient (req.swagger.params, res, next);
+};
+
+module.exports.putClient = function putClient (req, res, next) {
+    Client.putClient (req.swagger.params, res, next);
+};
+
+module.exports.postClient = function postClient (req, res, next) {
+    Client.postClient (req.swagger.params, res, next);
+};
+
+module.exports.patchClient = function patchClient (req, res, next) {
+    Client.patchClient (req.swagger.params, res, next);
+};
+
